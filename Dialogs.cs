0362c6b45f1a8ceb00fe74ea6d02c3814afbd75f
asdfCYBER <asdfCYBER@outlook.com>
2021-03-30 11:56:45 +0200

    Fix style issue

--- Dialogs.cs
+++ Dialogs.cs
@@ -72,7 +72,6 @@ namespace PPTXcreator
                 {
                     GenericWarning("Het geselecteerde bestand kan niet worden gevonden");
                 }
-                
             }
 
             return null;
