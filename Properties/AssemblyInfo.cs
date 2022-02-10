32b647293eb94175110b945c6a1b43c6421dc041
asdfCYBER <asdfCYBER@outlook.com>
2022-02-10 13:02:57 +0100

    Update dependencies

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PPTXcreator")]
-[assembly: AssemblyDescription("Maakt PowerPoint bestanden voor livestreams van Sionkerk Houten")]
+[assembly: AssemblyDescription("Maakt PowerPoint bestanden voor livestreams")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PPTXcreator")]
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 [assembly: NeutralResourcesLanguage("nl")]
